2b6e2e91e551535f8b17f41dee012015c87a1a14
Jonas Birkeli <jonabi@stud.ntnu.no>
2025-04-17 08:24:48 +0200

    Config for db created

--- config/db_config.py
+++ config/db_config.py
@@ -1,11 +1,1 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-# MySQL configuration for NTNU
-MYSQL_CONFIG = {
-    'host': os.getenv('DB_HOST', 'stupid_host_url'),
-    'user': os.getenv('DB_USER', 'stupid_username'),
-    'password': os.getenv('DB_PASSWORD', 'stupid_password'),
-    'database': os.getenv('DB_NAME', 'stupid_db_name')
-}
+DATABASE_FILE = 'fitness_tracker.db'
--- db/connection.py
+++ db/connection.py
@@ -1,7 +1,8 @@
 import sqlite3
+from config import db_config
 
 def get_db_connection():
     """Create and return a connection to the SQLite database"""
-    conn = sqlite3.connect('fitness_tracker.db')
+    conn = sqlite3.connect(db_config.DATABASE_FILE)
     conn.row_factory = sqlite3.Row
     return conn
